dea964c7b2dc40a93e91340289990e4e8c1a0e86
SarangAP <sp49@njit.edu>
2024-02-18 17:02:20 -0500

    Added input box fields

--- task-better/src/Components/Login/Login.jsx
+++ task-better/src/Components/Login/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './Login.css';
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className="wrapper">
+            <form action ="">
+                <h1>Login Here</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder = 'Username' required />
+                </div>
+
+                <div className='input-box'>
+                    <input type="password" placeholder = 'Password' required />
+                </div>
+            </form>
+        </div>
     )
 }
 
