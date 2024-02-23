99a388b951551101220e84ed7ddf3f558de74146
SarangAP <sp49@njit.edu>
2024-02-22 21:17:15 -0500

    Set MySQL information in settings.py

--- django-tb-react/djback/djback/settings.py
+++ django-tb-react/djback/djback/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_better',
+        'USER': 'root',
+        'PASSWORD': 'adminhere',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
