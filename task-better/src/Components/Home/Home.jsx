f1afd69fec272d2668e5e8d74ba77b3b17ab7a44
SarangAP <sp49@njit.edu>
2024-02-24 15:13:17 -0500

    Testing Home page

---
+++ task-better/src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Task Better</h1>
+      <p>This will serve as our homepage/dashboard</p>
+    </div>
+  );
+};
+
+export default Home;
--- task-better/src/Components/Login/Login.jsx
+++ task-better/src/Components/Login/Login.jsx
@@ -1,23 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import './Login.css';
+
+
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            console.log('Before fetch:', username, password);
+            const response = await fetch('http://127.0.0.1:8000/login/', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+            console.log('After fetch:', username, password);
+            if (response.ok) {
+                console.log('Login successful');
+                <h2>its working</h2>
+            } else {
+                const data = await response.json();
+                console.error(data.message);
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+    
+
     return (
         <div className="wrapper">
-            <form action ="">
+            <form>
                 <h1>Login Here</h1>
                 <div className='input-box'>
-                    <input type="text" placeholder = 'Username' required />
+                    <input
+                        type="text"
+                        placeholder='Username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
                 </div>
 
                 <div className='input-box'>
-                    <input type="password" placeholder = 'Password' required />
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => 
+                            setPassword(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className='submit-button'>
-                <button type="button">Log in</button>
+                    <button type="" onClick={handleLogin}>
+                        Log in
+                    </button>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
