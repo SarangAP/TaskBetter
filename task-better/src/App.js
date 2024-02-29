9d2e39ba02f2206642304a33f3eb7bbed52b2ff2
Romil Patel <romilpatel764@gmail.com>
2024-02-29 15:32:06 -0500

    Containers in App.js

--- task-better/src/App.js
+++ task-better/src/App.js
@@ -3,17 +3,35 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login'
 import Home from './Components/Home/Home';
+import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<LoginContainer />} />
+        <Route element={<DefaultContainer/>}> <Route path="/home" element={<Home />} /></Route>
       </Routes>
     </Router>
 
   );
 }
+const LoginContainer = () => (
+  <div className="container">
+    {/* <Route exact path="/" element={<Redirect to="/login" />} /> */}
+    <Route path="/login" element={<Login />} />
+  </div>
+);
+
+// Default Container (Includes navigation bar)
+const DefaultContainer = ({ children }) => (
+  <div>
+    <Nav/>
+    <div className="container">
+      {children}
+    </div>
+  </div>
+);
+
 
 export default App;
---
+++ task-better/src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Nav = () =>{
+    return (
+        <div>Task Better</div>
+    )
+
+}
+
+
+
+
+export default Nav;
