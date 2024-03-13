1b8b748f51f3265bfe630dd34a0f3f3f9439e2e6
Snow <b.mogul02@gmail.com>
2024-03-13 14:12:06 -0400

    Added template to check for session token

--- task-better/src/App.js
+++ task-better/src/App.js
@@ -5,37 +5,19 @@ import Login from "./Components/Login/Login";
 import Home from "./Components/Home/Home";
 import TaskPage from "./Components/Tasks/TaskPage";
 import Profile from "./Components/Profile";
-import TBNav from "./Components/TBNav/TBNav";
+import Authenticated from "./Components/Template/Authenticated";
 
 function App() {
-  useEffect(() => {
-    if (sessionStorage.getItem("token")) {
-      console.log("Session Token", sessionStorage.getItem("token"));
-    } else {
-        console.log("NO TOKEN")
-        window.location.href = "/"; 
-    }
-  }, []);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<DefaultContainer ><Home/></DefaultContainer>} />
-        <Route path="/tasks" element={<DefaultContainer><TaskPage /></DefaultContainer>} />
-        <Route path="/profile" element={<DefaultContainer><Profile /></DefaultContainer>} />
+        <Route path="/home" element={<Authenticated page={<Home/>}/>} />
+        <Route path="/tasks" element={<Authenticated page={<TaskPage />}/>} />
+        <Route path="/profile" element={<Authenticated page={<Profile />}/>} />
       </Routes>
     </Router>
   );
-
 }
-
-// Default Container (Includes navigation bar)
-const DefaultContainer = ({ currentUser, children }) => (
-  <div>
-    <TBNav currentUser={currentUser} />
-    {children}
-  </div>
-);
 
 export default App;
---
+++ task-better/src/Components/Template/Authenticated.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import TBNav from "../TBNav/TBNav";
+
+const Authenticated = ({ page }) => {
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      console.log("Session Token", sessionStorage.getItem("token"));
+    } else {
+      if (window.location.pathname !== "/") {
+        console.log("NO TOKEN", console.log(window.location.pathname));
+        window.location.href = "/";
+      }
+    }
+  }, []);
+  return (
+    <>
+      <TBNav />
+      {page}
+    </>
+  );
+};
+
+export default Authenticated;
