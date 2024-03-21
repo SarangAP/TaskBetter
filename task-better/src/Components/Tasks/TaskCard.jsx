4f88e14db5d1efc054d1497c8bf0a7a86f04283c
Snow <b.mogul02@gmail.com>
2024-03-21 15:10:46 -0400

    Fixed bugs for display tasks

--- task-better/src/Components/Tasks/TaskCard.jsx
+++ task-better/src/Components/Tasks/TaskCard.jsx
@@ -33,7 +33,7 @@ const TaskCard = ({ task, handleDelete }) => {
       {isRotated ? (
         <div className={`row ${isRotated ? "visible" : "invisible"}`}>
           <div className="col-sm text-start ms-1">
-            <p>{task.body}</p>
+            <p>{task.description}</p>
           </div>
         </div>
       ) : (
