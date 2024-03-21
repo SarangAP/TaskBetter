cbb7b6c0ce39f8c82e4158b431ba4fff7feb185f
Romil Patel <romilpatel764@gmail.com>
2024-03-21 13:40:39 -0400

    Added description about TaskBetter on the home page

--- task-better/src/Components/Home/Home.jsx
+++ task-better/src/Components/Home/Home.jsx
@@ -15,6 +15,7 @@ const Home = ({ currentUser }) => {
         <>
           <h1>Welcome {user.username} to Task Better</h1>
           <p>This will serve as our homepage/dashboard</p>
+          <p>TaskBetter is a platform where people are able to manage their tasks efficiently. With everyone's lives being pretty busy, it can be easy to lose track. Everyone lifestyles vary. TaskBetter also allows people to participate in a game where they can earn points after completing a task. Having this feature will let people have a fun experience with task management. TaskBetter would like for users to compete with other users and have fun with it. </p>
         </>
       ) : (
         <></>
