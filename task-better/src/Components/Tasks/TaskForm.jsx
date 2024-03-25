d3baf84ea0ed9261f6a38b92094ed4654aead6a3
SarangAP <sp49@njit.edu>
2024-03-25 10:48:47 -0400

    Added new due date selector

--- task-better/src/Components/Tasks/TaskForm.jsx
+++ task-better/src/Components/Tasks/TaskForm.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
 
 const TaskForm = ({ addTask }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [completed, setCompleted] = useState(false);
+  const [dueDate, setDueDate] = useState(new Date());
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Check if title and description are empty
-    if (title.trim() === "" || description.trim() === "") {
+    if (title.trim() === "" || description.trim() === "" || dueDate === "") {
       console.log("Title and description cannot be empty");
+      alert("Please fill out all the field");
       // return;
     } else {
       fetch("http://127.0.0.1:8000/tasks/", {
@@ -20,17 +23,19 @@ const TaskForm = ({ addTask }) => {
           "Content-Type": "application/json",
           Authorization: "Token " + sessionStorage.getItem("token"),
         },
-        body: JSON.stringify({ title, description, completed }),
+        body: JSON.stringify({ title, description, completed }), // will need to add due date here when django view is modified
       })
         .then((response) => response.json())
         .then((data) => {
           // Handle the response data
           console.log("New Task that is being added",data);
+          console.log("Selected due date", dueDate);
           if (Array.isArray(data.title) || Array.isArray(data.description))
             throw new Error("Invalid title or description");
           addTask(data);
           setTitle("");
           setDescription("");
+          setDueDate("");
         })
         .catch((error) => {
           // Handle any errors
@@ -66,6 +71,17 @@ const TaskForm = ({ addTask }) => {
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
         </div>
+        <div classname="mb-3">
+          <label> Set due date</label>
+          <input
+            type="date"
+            className="form-control"
+            id="dueDate"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+            />
+        </div>
+        <br></br>
         <button type="submit" className="btn btn-primary mx-auto">
           Submit
         </button>
