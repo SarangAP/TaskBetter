48de317a88af3af9cfc1f66c6b740392bcf51696
SarangAP <sp49@njit.edu>
2024-03-27 21:34:38 -0400

    Added image to footer

--- task-better/src/Components/Footer/Footer.jsx
+++ task-better/src/Components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import TaskBetter_Image from "../TBNav/TaskBetterLogo.svg"
 
 const Footer = () => {
     return (
       <footer className="footer">
         <div className="content">
+          <div className="logo">
+            <img src={TaskBetter_Image} alt="TaskBetter Logo" />
+          </div>
           <p>TaskBetter will include links to pages</p>
         </div>
       </footer>
