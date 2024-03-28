067844d5bf636f7314b634f1d480adebd27bacd1
SarangAP <sp49@njit.edu>
2024-03-28 14:06:28 -0400

    Added newsletter sign up

--- task-better/src/Components/Footer/Footer.jsx
+++ task-better/src/Components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
 import TaskBetter_Image from "../TBNav/TaskBetterLogo.svg"
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("You have successfully signed up!");
+    setEmail("");
+  }
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  }
+
     return (
       <footer className="footer">
         <div className="content">
@@ -20,6 +32,19 @@ const Footer = () => {
               <li><a href="#">Terms & Conditions</a></li>
             </ul>
           </nav>
+          <div className="newsletter">
+            <h5>Sign up for our newsletter</h5>
+            <form onSubmit={handleSubmit} >
+              <input type="email"
+              name="email"
+              placeholder="Enter your email here" 
+              value={email}
+              onChange={handleChange}
+              required
+              />
+              <button type="submit">Subscribe</button>
+            </form>
+          </div>
         </div>
         <div className="copyright">
         <p>© 2024 TaskBetter | All Rights Reserved</p>
