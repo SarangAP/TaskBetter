aa6835bf4b2c66e84b8af6482c4dc84f4a043c9e
SarangAP <sp49@njit.edu>
2024-04-06 22:04:06 -0400

    Added leaderboard table view

--- task-better/src/Components/Leaderboard/Leaderboard.jsx
+++ task-better/src/Components/Leaderboard/Leaderboard.jsx
@@ -2,10 +2,47 @@ import React, { useState, useEffect } from 'react';
 import "./Leaderboard.css";
 
 const Leaderboard = () => {
+  const [stats, setStats] = useState([]);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/leaderboard/');
+        if (!response.ok) {
+          throw new Error('The leaderboard fetch failed');
+        }
+        const userStats = await response.json();
+        
+        setStats(userStats);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchStats();
+  }, []);
 
   return (
     <div>
-        <p>This will be leaderboard page</p>
+      <h1>Task Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Tasks Completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td>{user.username}</td>
+              <td>{user.completed_tasks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
