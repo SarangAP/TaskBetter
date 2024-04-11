95275af4fac84cc865a56a556303b95a2da9a0da
Raymond-Moody <144750662+Raymond-Moody@users.noreply.github.com>
2024-04-11 18:27:18 -0400

    Profile editing

--- django-tb-react/djback/tb/views.py
+++ django-tb-react/djback/tb/views.py
@@ -114,6 +114,15 @@ class ProfileView(APIView):
         serializer = UserSerializer(user)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class LogoutView(APIView):
     authentication_classes = [TokenAuthentication]
