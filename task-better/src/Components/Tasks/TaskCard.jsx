7237a2dd492d114f6e53aa9b9c4eaa0e28711980
Snow <b.mogul02@gmail.com>
2024-04-11 17:20:14 -0400

    sends update req to backend

--- task-better/src/Components/Tasks/TaskCard.jsx
+++ task-better/src/Components/Tasks/TaskCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Trash from "../icons/Trash";
 import Caret from "../icons/Caret";
 import CheckBox from "../icons/CheckBox";
@@ -9,12 +9,27 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
   const [isRotated, setIsRotated] = useState(false);
   const [completed, setCompleted] = useState(task.completed);
   const [canEdit, setCanEdit] = useState(false);
+  const [desc, setDesc] = useState(task.desctiption);
+  const [due_date, setDueDate] = useState(task.due_date);
+  const [priority, setPriority] = useState(task.priority);
+
+  useEffect(() => {
+    setDesc(task.description || "");
+    setDueDate(task.due_date || "");
+    setPriority(task.priority || "");
+  }, [task]);
 
   const handleDropDown = () => {
     setCanEdit(false);
     setIsRotated(!isRotated);
   };
   const handleEdit = () => {
+    if (canEdit) {
+      task.description = desc
+      task.due_date = due_date
+      task.priority = priority
+      handleUpdate(task)
+    }
     setCanEdit(!canEdit);
     setIsRotated(true);
   };
@@ -22,6 +37,17 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
     task.completed = !completed;
     handleUpdate(task);
     setCompleted(!completed);
+  };
+  const handleDescChange = (e) => {
+    setDesc(e.target.value);
+  };
+
+  const handleDueDateChange = (e) => {
+    setDueDate(e.target.value);
+  };
+
+  const handlePriorityChange = (e) => {
+    setPriority(e.target.value);
   };
   return (
     <div className="container task-card bg-light rounded-2 p-2 w-100 mb-3">
@@ -50,28 +76,22 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
               <label>Description: </label>
               <input
                 className="form-control"
-                value={task.description}
-                onChange={(e) => {}}
+                value={desc}
+                onChange={handleDescChange}
               />
               <label>Due Date: </label>
               <input
                 type="date"
                 className="form-control"
-                value={task.due_date}
-                onChange={(e) => {}}
+                value={due_date}
+                onChange={handleDueDateChange}
               />
               <label>Priority: </label>
               <input
                 className="form-control"
-                value={task.priority}
-                onChange={(e) => {}}
-              />
-              <label>Completed: </label>
-              <input
-                className="form-control"
-                value={task.completed ? "True" : "False"}
-                onChange={(e) => {}}
-              />
+                value={priority}
+                onChange={handlePriorityChange}
+              />{" "}
               <p>
                 Task created:{" "}
                 {moment(task.created).format("MMMM DD, YYYY h:mm:ss A")}
@@ -84,7 +104,6 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
               <p>Description: {task.description}</p>
               <p>Due date: {moment(task.due_date).format("MM/DD/YYYY")}</p>
               <p>Priority: {task.priority}</p>
-              <p>Completed: {task.completed.toString()}</p>
               <p>
                 Task created:{" "}
                 {moment(task.created).format("MMMM DD, YYYY h:mm:ss A")}
