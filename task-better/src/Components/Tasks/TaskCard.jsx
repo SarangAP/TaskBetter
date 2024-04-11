ac5da84e41a750a3d977df2fa48f9518c5e586b7
Snow <b.mogul02@gmail.com>
2024-04-11 17:04:17 -0400

    edit view for taskcard

--- task-better/src/Components/Tasks/TaskCard.jsx
+++ task-better/src/Components/Tasks/TaskCard.jsx
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import Trash from "../icons/Trash";
 import Caret from "../icons/Caret";
 import CheckBox from "../icons/CheckBox";
-import moment from "moment"
+import Editbox from "../icons/Editbox";
+import moment from "moment";
 
 const TaskCard = ({ task, handleDelete, handleUpdate }) => {
   const [isRotated, setIsRotated] = useState(false);
   const [completed, setCompleted] = useState(task.completed);
+  const [canEdit, setCanEdit] = useState(false);
 
   const handleDropDown = () => {
+    setCanEdit(false);
     setIsRotated(!isRotated);
   };
+  const handleEdit = () => {
+    setCanEdit(!canEdit);
+    setIsRotated(true);
+  };
   const handleComplete = () => {
-    task.completed = !completed
-    handleUpdate(task)
+    task.completed = !completed;
+    handleUpdate(task);
     setCompleted(!completed);
   };
   return (
@@ -22,11 +29,14 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
         <div className="col-sm-1">
           <CheckBox handleComplete={handleComplete} isCompleted={completed} />
         </div>
-        <div className="col-sm-8">
+        <div className="col-sm-7">
           <h5 className="text-truncate expand-on-hover">{task.title}</h5>
         </div>
         <div className="col-sm-1">
           <Caret isRotated={isRotated} handleDropDown={handleDropDown} />
+        </div>
+        <div className="col-sm-1">
+          <Editbox handleDropDown={handleEdit} />
         </div>
         <div className="col-sm-1">
           <Trash handleDelete={handleDelete} task={task} />
@@ -34,15 +44,54 @@ const TaskCard = ({ task, handleDelete, handleUpdate }) => {
       </div>
 
       {isRotated ? (
-        <div className={`row ${isRotated ? "visible" : "invisible"}`}>
-          <div className="col-sm text-start ms-1">
-            <p>Description: {task.description}</p>
-            <p>Due date: {moment(task.due_date).format("MM/DD/YYYY")}</p>
-            <p>Priority: {task.priority}</p>
-            <p>Completed: {task.completed.toString()}</p>
-            <p>Task created: {moment(task.created).format("MMMM DD, YYYY h:mm:ss A")}</p>
+        canEdit ? (
+          <div className={`row ${isRotated ? "visible" : "invisible"}`}>
+            <div className="col-sm text-start ms-1">
+              <label>Description: </label>
+              <input
+                className="form-control"
+                value={task.description}
+                onChange={(e) => {}}
+              />
+              <label>Due Date: </label>
+              <input
+                type="date"
+                className="form-control"
+                value={task.due_date}
+                onChange={(e) => {}}
+              />
+              <label>Priority: </label>
+              <input
+                className="form-control"
+                value={task.priority}
+                onChange={(e) => {}}
+              />
+              <label>Completed: </label>
+              <input
+                className="form-control"
+                value={task.completed ? "True" : "False"}
+                onChange={(e) => {}}
+              />
+              <p>
+                Task created:{" "}
+                {moment(task.created).format("MMMM DD, YYYY h:mm:ss A")}
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className={`row ${isRotated ? "visible" : "invisible"}`}>
+            <div className="col-sm text-start ms-1">
+              <p>Description: {task.description}</p>
+              <p>Due date: {moment(task.due_date).format("MM/DD/YYYY")}</p>
+              <p>Priority: {task.priority}</p>
+              <p>Completed: {task.completed.toString()}</p>
+              <p>
+                Task created:{" "}
+                {moment(task.created).format("MMMM DD, YYYY h:mm:ss A")}
+              </p>
+            </div>
+          </div>
+        )
       ) : (
         <></>
       )}
