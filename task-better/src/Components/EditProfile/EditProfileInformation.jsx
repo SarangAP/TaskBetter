a0296642669af115e88245a474512e1c5eb09d22
rp855 <rp855@njit.edu>
2024-04-14 17:09:01 -0400

    worked on Edit profile

--- task-better/src/Components/EditProfile/EditProfileInformation.jsx
+++ task-better/src/Components/EditProfile/EditProfileInformation.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import "./Profile.css";
+import "./EditProfileInformation.css"
 import { Link } from "react-router-dom";
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Profile = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser("");
+    toast("You have successfully changed your user info!")
+  }
   useEffect(() => {
     const getProfile = async () => {
       try {
@@ -40,33 +46,39 @@ const Profile = () => {
   return (
     <div>
       <div className="profile-container">
-      <div className="welcome-profile"><h2>Welcome to your Profile Page</h2></div>
+      <div className="welcome-profile"><h2>You may edit your information here</h2></div>
       {loading ? (
         <p>Loading profile info now</p> //sometimes doesn't load immediately
       ) : user ? (
         <div className="user-information">
-          
-            <Link to="/editprofile" className="btn btn-secondary">Edit Profile Information</Link>
+                      <Link to="/profile" className="btn btn-secondary">Back to Profile Page</Link>
+
           
           <div className="user-info">
             <p><b>User ID: </b> {user.id}</p>
           </div>
           <div className="user-info">
-            <p><b>Username: </b>{user.username}</p>
+            <p><b>Username:</b> <input type="text" name="username"/></p>
+            
           </div>
           <div className="user-info">
-            <p><b>First Name: </b>{user.first_name}</p>
+            <p><b>First Name: </b> <input type="text" name="firstname"/></p>
            </div>
           <div className="user-info">
-            <p><b>Last Name: </b>{user.last_name}</p>
+            <p><b>Last Name: </b><input type="text" name="lastname"/></p>
           </div>
           <div className="user-info">
-            <p><b>Email: </b>{user.email}</p>
+            <p><b>Email: </b><input type="text" name="email"/></p>
           </div>
-          
+          <div className="user-info">
+            <p><b>Password: </b><input type="text" name="password"/></p>
+          </div>
           <div className="user-info">
             <p><b>Date joined:</b> {user.date_joined}</p>
           </div>
+          <button type="submit" className="btn btn-primary mx-auto" onSubmit={handleSubmit}>
+          Submit
+        </button>
         </div>
       ) : (
         <p>Error loading profile</p>
